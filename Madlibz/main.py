9b6d80f759bb0d0d04af82d1c1b9f9cda282af3e
Dominik Broder <dominik.b.broder@gmail.com>
2022-03-15 10:58:29 +0100

    Madlibz

---
+++ Madlibz/main.py
@@ -0,0 +1,7 @@
+word_one = input("Enter a color:")
+word_two = input("Enter a plural:")
+word_tree = input("Enter an animal:")
+
+print("Roses are", word_one)
+print(word_two + " are blue ")
+print("I like", word_tree)
